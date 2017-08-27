e3516b2a8893f53065bbad21c49704c1a34d04df
chate <chatethakhun.jack@hotmail.com>
2017-08-27 10:19:59 +0700

    i18n in login page

--- public/js/language.js
+++ public/js/language.js
@@ -1,12 +1,7 @@
 function updateText() {
 	'use strict';
-	var i18n = $.i18n(), intro, start, language;
-	intro = $( '#intro' ).text();
-	start = $( '#start' ).text();
+	var i18n = $.i18n(), language;
 	language = $( '#language option:selected' ).val();
-	console.log("H1 " + intro);
-	console.log("Start Button " + start);
-	console.log("Language " + language);
 	/*message = '$1 has $2 {{plural:$2|kitten|kittens}}. '
 		+ '{{gender:$3|He|She}} loves to play with {{plural:$2|it|them}}.';
 
@@ -19,13 +14,16 @@ function updateText() {
 	i18n.locale = language;
 	i18n.load( '../json-languages/' + i18n.locale + '.json', i18n.locale ).done(
 		function(e) {
-			console.log(e);
 			//var personName = $.i18n( person ), localizedMessage = $.i18n( message, personName,
 				//kittens, gender, test );
 				//console.log(personName);
 				//console.log(localizedMessage);
 			$( '#intro' ).text( e.intro );
 			$( '#start' ).text( e.start );
+			$( '#login' ).text( e.login );
+			$( '#loginBtn' ).text( e.login );
+			$('#username').attr("placeholder", e.username);
+			$('#password').attr("placeholder", e.password);
 		} );
 }
 // Enable debug
