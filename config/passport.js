a693370c36b68911d8e7b0d0b1965d48d6566cee
chate <chatethakhun.jack@hotmail.com>
2017-09-02 03:10:11 +0700

    fix bug

--- config/passport.js
+++ config/passport.js
@@ -36,8 +36,7 @@ module.exports = function(passport, url) {
         // pull in our app id and secret from our auth.js file
         clientID        : configAuth.facebookAuth.clientID,
         clientSecret    : configAuth.facebookAuth.clientSecret,
-        callbackURL     : configAuth.facebookAuth.callbackURL,
-        profileFields: ['id','username', 'displayName', 'link', 'about_me', 'photos', 'email']
+        callbackURL     : configAuth.facebookAuth.callbackURL
 
     },
 
