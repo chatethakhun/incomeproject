967ce27b386311749f6a484c82c21c03e738394e
chate <chatethakhun.jack@hotmail.com>
2017-09-02 02:44:46 +0700

    fix url facebook

--- config/auth.js
+++ config/auth.js
@@ -4,6 +4,6 @@ module.exports = {
   'facebookAuth' : {
       'clientID'      : '208045423063222', // your App ID
       'clientSecret'  : '7ddaee180d20070f4e242d22d2a2f4c3', // your App Secret
-      'callbackURL'   : 'http://localhost:5000/auth/facebook/callback'
+      'callbackURL'   : 'https://incomeproject.herokuapp.com/auth/facebook/callback'
   }
 }
